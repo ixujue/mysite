5fac9d17ee035b6fa7c3713149af86182e23f217
520734 <ixujue@163.com>
2025-03-05 22:18:27 +0800

    修改首页和目录结构

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '我的博客';
+export const SITE_TITLE = '哪里不会';
 export const SITE_DESCRIPTION = '欢迎访问我的个人网站！';
